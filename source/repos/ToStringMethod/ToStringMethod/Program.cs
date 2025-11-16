232c817037ab4f3e776485dc8a6088fe56c7fe51
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2025-11-16 21:16:27 +0100

    DbContext

---
+++ source/repos/ToStringMethod/ToStringMethod/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ToStringMethod
+{
+    internal class Program
+    {
+        //ToString()
+        // ToString method converts an object to it's string representation for suitable display
+        static void Main(string[] args)
+        {
+            Car car = new Car("Toyota", "BigDaddy", "Red");
+
+            Console.WriteLine(car);
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+    class Car
+    {
+        string make;
+        string model;
+        string color;
+
+        public Car(string make, string model, string color)
+        {
+
+            this.make = make;
+            this.model = model;
+            this.color = color;
+        }
+        public override string ToString()
+        {
+            return "This is a " + make +" " + model + " " + color +" "+ "color";
+        }
+    }
+
+    
+      
+
+    
+}
