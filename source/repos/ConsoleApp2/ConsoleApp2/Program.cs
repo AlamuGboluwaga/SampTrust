70e19e34a84b141b1cb0e9f444fd3a45959cec70
Gboluwaga Alamu <alamugboluwaga402@gmail.com>
2025-11-22 13:42:51 +0100

    dbContext

---
+++ source/repos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 100; i++) { 
+            
+            if(i<50)
+                {
+                    Console.WriteLine($"Student has failed with  scores = {i}");
+                }
+            else if(i >= 50 ){
+                    Console.WriteLine($"Student passed with   scores = {i}");
+                }
+            
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
